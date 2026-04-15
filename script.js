dacadd697a9e8dd3f96f3de8c8bec2484b65be11
Aldo Sibilio <sibilioaldo6@gmail.com>
2026-04-15 16:18:35 +0200

    aggiunta chiamata api

--- script.js
+++ script.js
@@ -4,4 +4,15 @@ const emailList = document.querySelector("#email-list");
 // ciclo 10 volte
 for (let i = 0; i < 10; i++) {
 
+    // chiamata API
+    fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+
+        .then(function (response) {
+            return response.json();
+        })
+
+        .then(function (data) {
+
+        });
+
 }
